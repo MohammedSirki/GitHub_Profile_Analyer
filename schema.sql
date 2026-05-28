ffa2a01fbcaa27540f4e6b43cfa0a0d3f4780b5a
unknown <mohammedserki123@gmail.com>
2026-05-28 13:40:08 +0530

    Added database schema and Postman collection

---
+++ schema.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE github_analyzer;
+USE github_analyzer;
+
+CREATE TABLE github_users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(100) UNIQUE,
+  name VARCHAR(150),
+  bio TEXT,
+  followers INT,
+  following_count INT,
+  public_repos INT,
+  profile_url VARCHAR(255),
+  avatar_url VARCHAR(255),
+  created_at_github DATETIME,
+  analyzed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE github_analysis (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(100),
+  total_stars INT,
+  total_forks INT,
+  top_language VARCHAR(100),
+  languages_used TEXT,
+  top_repo VARCHAR(150),
+  developer_type VARCHAR(100),
+  profile_score INT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
